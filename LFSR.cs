a650c66d09e862ab7372a90695193f7429097e58
Gamatoza <tihonvlasov2014@gmail.com>
2021-04-16 13:03:01 +0300

    lsfr++

---
+++ LFSR.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EncryptionLib;
+
+namespace EncryptionLib.LFSR
+{
+    public class LFSR:ICryptoBasic
+    {
+
+        public string Key { get; set; } = "x^27 + x^8 + x^7 + x + 1"; //formula //def: x^27 + x^8 + x^7 + x + 1
+        
+        
+
+        public string Input { get; set; } //text //byte
+
+        public string Extra => "";
+
+        public string Decode()
+        {
+            return "";
+        }
+
+        public string Encode()
+        {
+
+            return "";
+        }
+
+        Byte[] AllIntoBytes(string path) => System.IO.File.ReadAllBytes(@"" + path);
+
+    }
+}
